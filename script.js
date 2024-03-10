b426a94d7806f76b08289dd6163180c0e839a788
amelie <ameliemasson78@gmail.com>
2024-03-10 13:47:03 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+let home = document.getElementById("home");
+let about = document.getElementById("about");
+let design = document.getElementById("design");
+let art = document.getElementById("art");
+let contact = document.getElementById("contact");
+
+home.addEventListener("click", function () {
+  console.log("vous avez cliqué sur home");
+  window.location.href = "index.html";
+});
+
+about.addEventListener("click", function () {
+  window.location.href = "about.html";
+});
+
+design.addEventListener("click", function () {
+  window.location.href = "design.html";
+});
+
+art.addEventListener("click", function () {
+  window.location.href = "art.html";
+});
+
+contact.addEventListener("click", function () {
+  window.location.href = "contact.html";
+});
