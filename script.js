6efca34b56460d2557a5f0593eb87c0c52ddea40
amelie <ameliemasson78@gmail.com>
2024-03-30 15:51:45 +0100

    refonte + section ABOUT + section DESIGN

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
-let home = document.getElementById("home");
-let about = document.getElementById("about");
-let design = document.getElementById("design");
-let art = document.getElementById("art");
-let contact = document.getElementById("contact");
+// let home = document.getElementById("home");
+// let about = document.getElementById("about");
+// let design = document.getElementById("design");
+// let art = document.getElementById("art");
+// let contact = document.getElementById("contact");
 
-home.addEventListener("click", function () {
-  console.log("vous avez cliqué sur home");
-  window.location.href = "index.html";
-});
+// home.addEventListener("click", function () {
+//   console.log("vous avez cliqué sur home");
+//   window.location.href = "index.html";
+// });
 
-about.addEventListener("click", function () {
-  window.location.href = "about.html";
-});
+// about.addEventListener("click", function () {
+//   window.location.href = "about.html";
+// });
 
-design.addEventListener("click", function () {
-  window.location.href = "design.html";
-});
+// design.addEventListener("click", function () {
+//   window.location.href = "design.html";
+// });
 
-art.addEventListener("click", function () {
-  window.location.href = "art.html";
-});
+// art.addEventListener("click", function () {
+//   window.location.href = "art.html";
+// });
 
-contact.addEventListener("click", function () {
-  window.location.href = "contact.html";
-});
+// contact.addEventListener("click", function () {
+//   window.location.href = "contact.html";
+// });
