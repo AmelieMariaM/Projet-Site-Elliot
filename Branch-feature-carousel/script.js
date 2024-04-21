f98b665524cf3b035529111c95a973c01c089753
amelie <ameliemasson78@gmail.com>
2024-04-21 15:29:22 +0200

    branch carousel created and clean-up for first pull request attempt

---
+++ Branch-feature-carousel/script.js
@@ -0,0 +1,68 @@
+// let home = document.getElementById("home");
+// let about = document.getElementById("about");
+// let design = document.getElementById("design");
+// let art = document.getElementById("art");
+// let contact = document.getElementById("contact");
+
+// home.addEventListener("click", function () {
+//   console.log("vous avez cliqué sur home");
+//   window.location.href = "index.html";
+// });
+
+// about.addEventListener("click", function () {
+//   window.location.href = "about.html";
+// });
+
+// design.addEventListener("click", function () {
+//   window.location.href = "design.html";
+// });
+
+// art.addEventListener("click", function () {
+//   window.location.href = "art.html";
+// });
+
+// contact.addEventListener("click", function () {
+//   window.location.href = "contact.html";
+// });
+
+// const chocoButton = document.querySelector("#chocoButton");
+// const milkButton = document.querySelector("#milkButton");
+// const mixButton = document.querySelector("#mixButton");
+// const cleanButton = document.querySelector("#cleanButton");
+// const machine = document.querySelector("#machine");
+
+// // Add events bellow:
+
+// chocoButton.addEventListener("click", function () {
+//   machine.innerText += "🍫";
+// });
+// milkButton.addEventListener("click", function () {
+//   machine.innerText += "🥛";
+// });
+
+// cleanButton.addEventListener("click", function () {
+//   machine.innerText = "";
+// });
+
+// mixButton.addEventListener("click", function () {
+//   let content = machine.textContent;
+//   let chocolate = 0;
+//   let milk = 0;
+//   console.log(machine.textContent);
+
+//   content.split("").forEach(function (character) {
+//     if (character === "🍫") {
+//       chocolate++;
+//     }
+//     if (character === "🥛") {
+//       milk++;
+//     }
+//     console.log(chocolate);
+//   });
+
+//   console.log(content);
+//   console.log(chocolate);
+//   console.log(milk);
+//   let pairs = Math.min(chocolate, milk);
+//   console.log(pairs);
+// });
